78b5c4b0133d2ea86fb7e50be41d67f3c879b5f4
mihailks <mihail_slavov@icloud.com>
2022-08-08 19:54:27 +0300

    changes to the last problem

--- CodinBat/String-3/notReplace.java
+++ CodinBat/String-3/notReplace.java
@@ -11,11 +11,14 @@ public class notReplace {
         String result = "";
         int length = str.length();
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'i' && str.charAt(i + 1) == 's' && !Character.isDigit(str.charAt(i + 2))) {
-                result += "is not";
-            } else {
+            if (i - 1 >= 0 && Character.isLetter(str.charAt(i - 1)) || i + 2 < length && Character.isLetter(str.charAt(i + 2))) {
                 result += str.charAt(i);
-            }
+
+            } else if (i + 1 < length && str.substring(i, i + 2).equals("is")) {
+                result += "is not";
+                i++;
+
+            } else result += str.charAt(i);
         }
         return result;
     }
