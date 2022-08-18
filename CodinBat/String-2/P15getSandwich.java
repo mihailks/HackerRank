d66ec251b00014539310c8791b5522b05e70eb1f
Mihail Slavov <mihail_slavov@icloud.com>
2022-08-18 16:32:44 +0300

    -

---
+++ CodinBat/String-2/P15getSandwich.java
@@ -0,0 +1,14 @@
+public class P15getSandwich {
+    public static void main(String[] args) {
+        String str = "xxbreadjambreadyy";
+        System.out.println(getSandwich(str));
+    }
+
+    public static String getSandwich(String str) {
+
+        String result = "";
+
+        return result;
+    }
+
+}
