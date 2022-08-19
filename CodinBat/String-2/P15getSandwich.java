13209f6fce5ad4e2dc7401402946b90bd3a39f68
Mihail Slavov <mihail_slavov@icloud.com>
2022-08-19 15:35:20 +0300

    -

--- CodinBat/String-2/P15getSandwich.java
+++ CodinBat/String-2/P15getSandwich.java
@@ -5,10 +5,19 @@ public class P15getSandwich {
     }
 
     public static String getSandwich(String str) {
-
-        String result = "";
-
-        return result;
+        int f = -1;
+        int l = -1;
+        for(int i=0; i < str.length()-4; i++) {
+            if(str.substring(i,i+5).equals("bread")) {
+                if(f==-1) {
+                    f=i;
+                }
+                l=i;
+            }
+        }
+        if( f == l) {
+            return "";
+        }
+        return str.substring(f+5,l);
     }
-
 }
