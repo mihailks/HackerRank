4d1c44f8858adef3ced23cc4fa4e437d3b334c65
Mihail Slavov <mihail_slavov@icloud.com>
2022-09-02 16:30:49 +0300

    -

---
+++ CodinBat/String-2/P21wordEnds.java
@@ -0,0 +1,13 @@
+public class P21wordEnds {
+    public static void main(String[] args) {
+        String str = "abcXY123XYijk";
+        String word = "XY";
+    }
+    public String wordEnds(String str, String word) {
+        String result="";
+
+
+
+        return result;
+    }
+}
