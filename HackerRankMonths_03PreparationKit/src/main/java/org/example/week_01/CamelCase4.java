535a1e3f0d9c1df12c25b471b5608eeaa57389e8
Mihail Slavov <mihail_slavov@icloud.com>
2024-01-05 18:45:38 +0200

    fix

--- HackerRankMonths_03PreparationKit/src/main/java/org/example/week_01/CamelCase4.java
+++ HackerRankMonths_03PreparationKit/src/main/java/org/example/week_01/CamelCase4.java
@@ -15,15 +15,53 @@ public class CamelCase4 {
                     .collect(Collectors.toList());
 
             if (input.get(0).equals("S")) {
-                convertWords(input.get(1), input.get(2));
+                splitWords(input.get(2));
             } else if (input.get(0).equals("C")) {
-
+                combineWords(input.get(1), input.get(2));
             }
         }
     }
 
-    private static void convertWords(String type, String words) {
+    private static void combineWords(String type, String words) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] arr = words.split(" ");
 
+        for (String s : arr) {
+            String output = s.substring(0, 1).toUpperCase() + s.substring(1);
+            stringBuilder.append(output);
+        }
+
+        if (type.equals("V") || type.equals("M")) {
+            String temp = stringBuilder.toString();
+            String output = temp.substring(0, 1).toLowerCase() + temp.substring(1);
+            stringBuilder = new StringBuilder(output);
+        }
+
+        if (type.equals("M")) {
+            stringBuilder.append("()");
+        }
+        System.out.println(stringBuilder);
+    }
+
+    private static void splitWords(String words) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < words.length(); i++) {
+            if (i == 0) {
+                stringBuilder.append(words.charAt(i));
+                continue;
+            }
+            if (words.charAt(i) >= 65 && words.charAt(i) <= 90) {
+                stringBuilder.append(" ");
+            }
+            if (words.charAt(i) == '(' || words.charAt(i) == ')') {
+                continue;
+            }
+
+            stringBuilder.append(words.charAt(i));
+        }
+
+        System.out.println(stringBuilder.toString().toLowerCase());
     }
 
 }
